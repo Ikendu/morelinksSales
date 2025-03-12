4896db2c5219f8ee5e5ccba4f534cc1e8c76170f
Chibundu <ndubest56@gmail.com>
2025-03-12 20:07:34 +0100

    more on main page

--- sidebar/sidebar.php
+++ sidebar/sidebar.php
@@ -4,13 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="sidebar.css">
+    <link rel="stylesheet" href="sidebar.css?v=<?php echo time(); ?>">
     <title>Document</title>
 </head>
 
 <body>
     <section class="category">
-        Category section
+        <div class="headCategory">All Category</div>
+        <ul class="navbar item">
+            <li><a href="index.html">Furniture</a></li>
+            <li><a href="about.html">Cusion</a></li>
+            <li><a href="services.html">Tables</a></li>
+            <li><a href="products.html">Living room</a></li>
+            <li><a href="contact.html">Dining table</a></li>
+            <li><a href="news.html">Bedroom</a></li>
+            <li><a href="news.html">Office</a></li>
+            <li><a href="news.html">Outdoor</a></li>
+            <li><a href="news.html">Kitchen</a></li>
+            <li><a href="news.html">Bathroom</a></li>
+            <li><a href="news.html">Kids conner</a></li>
+            <li><a href="news.html">Lighting</a></li>
+            <li><a href="news.html">Decor</a></li>
+
+        </ul>
     </section>
 </body>
 
