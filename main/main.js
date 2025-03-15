f2bd1d6028486bb9acdeb3463c1f374e23888e72
Chibundu <ndubest56@gmail.com>
2025-03-15 16:42:21 +0100

    goods display

--- main/main.js
+++ main/main.js
@@ -1,3 +1,20 @@
-$('.carousel').carousel({
-  interval: 2000,
-})
+let btnPlus = document.getElementById('btnPlus')
+btnPlus.addEventListener('click', () => {})
+let numberOfGoods = 0
+
+function addMore(id) {
+  numberOfGoods += 1
+  let count = document.getElementById(id)
+  count.innerHTML = numberOfGoods
+
+  console.log(numberOfGoods)
+}
+
+function removeGoods(id) {
+  if (numberOfGoods > 0) {
+    numberOfGoods -= 1
+    let count = document.getElementById(id)
+    count.innerHTML = numberOfGoods
+  }
+  console.log(numberOfGoods)
+}
--- main/maincontent.php
+++ main/maincontent.php
@@ -16,6 +16,7 @@
             <p>Visit different category to navigate your choice destination</p>
         </div>
         <section class="goods">
+            <!-- Display one -->
             <div class="goodsdisplay">
                 <img src="utilities/images/2.jpg" alt="">
                 <div class="discription">
@@ -24,9 +25,9 @@
                     <div class="price">
                         <h5>₦ 200,000</h5>
                         <div class="addMore">
-                            <button>+</button>
-                            <span>1</span>
-                            <button>-</button>
+                            <button onclick="addMore('one')">+</button>
+                            <span id="one">0</span>
+                            <button onclick="removeGoods('one')">-</button>
                         </div>
                     </div>
                     <div class="btnSales">
@@ -40,6 +41,9 @@
                     </div>
                 </div>
             </div>
+            <!-- End of Display one -->
+
+            <!-- Display second -->
             <div class="goodsdisplay">
                 <img src="utilities/images/15.jpg" alt="">
                 <div class="discription">
@@ -48,9 +52,9 @@
                     <div class="price">
                         <h5>₦ 400,000</h5>
                         <div class="addMore">
-                            <button>+</button>
-                            <span>1</span>
-                            <button>-</button>
+                            <button onclick="addMore('two')">+</button>
+                            <span id='two'>0</span>
+                            <button onclick="removeGoods('two')">-</button>
                         </div>
                     </div>
                     <div class="btnSales">
@@ -64,9 +68,123 @@
                     </div>
                 </div>
             </div>
+            <!-- End of Display second -->
+
+            <!-- Display Third -->
+            <div class="goodsdisplay">
+                <img src="utilities/images/11.jpg" alt="">
+                <div class="discription">
+                    <h4>Durable Fit</h4>
+                    <p>Sit out space</p>
+                    <div class="price">
+                        <h5>₦ 200,000</h5>
+                        <div class="addMore">
+                            <button onclick="addMore('three')">+</button>
+                            <span id="three">0</span>
+                            <button onclick="removeGoods('three')">-</button>
+                        </div>
+                    </div>
+                    <div class="btnSales">
+                        <button class="reachUsBtn">
+                            <img src="utilities/icons/whatsapp.png" alt="More-Link Whatsapp">
+                            <span>Reach us for this</span>
+                        </button>
+                        <button class="cartBtn">
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- End of Display Third -->
+
+            <!-- Display fourth -->
+            <div class="goodsdisplay">
+                <img src="utilities/images/3.jpg" alt="">
+                <div class="discription">
+                    <h4>Excel Space</h4>
+                    <p>Living room / cushion / decoration</p>
+                    <div class="price">
+                        <h5>₦ 4,000,000</h5>
+                        <div class="addMore">
+                            <button onclick="addMore('four')">+</button>
+                            <span id="four">0</span>
+                            <button onclick="removeGoods('four')">-</button>
+                        </div>
+                    </div>
+                    <div class="btnSales">
+                        <button class="reachUsBtn">
+                            <img src="utilities/icons/whatsapp.png" alt="More-Link Whatsapp">
+                            <span>Reach us for this</span>
+                        </button>
+                        <button class="cartBtn">
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- End of Display fourth -->
+
+
+            <!-- Display fifth -->
+            <div class="goodsdisplay">
+                <img src="utilities/images/8.jpg" alt="">
+                <div class="discription">
+                    <h4>Excel Space</h4>
+                    <p>Living room / cushion / decoration</p>
+                    <div class="price">
+                        <h5>₦ 4,000,000</h5>
+                        <div class="addMore">
+                            <button onclick="addMore('five')">+</button>
+                            <span id="five">0</span>
+                            <button onclick="removeGoods('five')">-</button>
+                        </div>
+                    </div>
+                    <div class="btnSales">
+                        <button class="reachUsBtn">
+                            <img src="utilities/icons/whatsapp.png" alt="More-Link Whatsapp">
+                            <span>Reach us for this</span>
+                        </button>
+                        <button class="cartBtn">
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- End of Display fifth -->
+
+            <!-- Display sixth -->
+            <div class="goodsdisplay">
+                <img src="utilities/images/9.jpg" alt="">
+                <div class="discription">
+                    <h4>Excel Space</h4>
+                    <p>Living room / cushion / decoration</p>
+                    <div class="price">
+                        <h5>₦ 4,000,000</h5>
+                        <div class="addMore">
+                            <button id="btnPlus">+</button>
+                            <span id="six">0</span>
+                            <button onclick="removeGoods('six')">-</button>
+                        </div>
+                    </div>
+                    <div class="btnSales">
+                        <button class="reachUsBtn">
+                            <img src="utilities/icons/whatsapp.png" alt="More-Link Whatsapp">
+                            <span>Reach us for this</span>
+                        </button>
+                        <button class="cartBtn">
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- End of Display sixth -->
+
+
+
         </section>
     </div>
 
+    <script src="main.js"></script>
 </body>
 
 </html>
