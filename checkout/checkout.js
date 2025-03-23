4d1b006fff76d8994d9b06ba1799b606fb0d3213
Chibundu <ndubest56@gmail.com>
2025-03-23 08:38:14 +0100

    checkout page processing

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,5 +1,5 @@
 let cart = JSON.parse(localStorage.getItem('cart')) || []
-console.log(cart)
+console.log('from storage', cart)
 function updateCheckoutPage() {
   let cartItemsContainer = document.getElementById('cart-items')
   let totalPrice = 0
@@ -9,7 +9,7 @@ function updateCheckoutPage() {
     totalPrice += item.price * item.quantity
     cartItemsContainer.innerHTML += `
                     <div class="cart-item">
-                        <img src="${item.image}" alt="${item.name}">
+                        <img src="../utilities/images/${item.image}" alt="${item.name}">
                         <div class="cart-item-info">
                             <span>${item.name} - $${item.price.toFixed(2)}</span>
                             <div>
@@ -23,3 +23,38 @@ function updateCheckoutPage() {
   })
   document.getElementById('total-price').innerText = totalPrice.toFixed(2)
 }
+
+// Updating the number of product using the plus and minus buttons
+function updateProductQuantity(index, change) {
+  if (cart[index]) {
+    cart[index].quantity += change
+    if (cart[index].quantity <= 0) {
+      cart.splice(index, 1)
+    }
+  }
+  localStorage.setItem('cart', JSON.stringify(cart))
+  updateCheckoutPage()
+}
+
+// Processing the payment with javascript
+function processPayment() {
+  console.log('Processing payment')
+  let firstname = document.getElementById('firstname').value
+  let lastname = document.getElementById('lastname').value
+  let phone = document.getElementById('phone').value
+  let email = document.getElementById('email').value
+  let address = document.getElementById('address').value
+  let town = document.getElementById('town').value
+  //   let paymentMethod = document.getElementById('payment-method').value
+
+  if (!firstname || !lastname || !phone || !email || !address) {
+    alert('Please fill in all required details.')
+    return
+  }
+
+  //   alert(`Processing payment via ${paymentMethod}...`)
+  localStorage.removeItem('cart')
+  window.location.href = 'confirmation.html'
+}
+
+updateCheckoutPage()
