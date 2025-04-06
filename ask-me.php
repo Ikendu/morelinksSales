f809c9559fac78df3c877a14e40b9c0c71d63039
Chibundu <ndubest56@gmail.com>
2025-04-06 11:57:07 +0100

    fqa section

--- ask-me.php
+++ ask-me.php
@@ -12,32 +12,31 @@
   <div class="askhead">
     <?php include "header/header.php" ?>
   </div>
-  <div class="advert"><?php include "sidebar/sidebar.php" ?></div>
-  <div class="askpage">
+  <div class="advert">
+    <?php include "sidebar/sidebar.php" ?>
+  </div>
+  <div class="askmain">
     <div class="askcontainer">
       <div class="askhead">
         <h2>Frequently Ask Questions</h2>
-        <p>Checkout our list of Frequently asked questions and see if you get the anwser you are looking for</p>
+        <p class="subtext">Checkout our list of Frequently asked questions and see if you get the anwser you are looking for</p>
       </div>
-      <section>
-        <h3>General Enqueries</h3>
-        <?php include_once "faq/general.html" ?>
-      </section>
+      <div class="askpage">
+        <section>
+          <?php include_once "faq/general.html" ?>
+        </section>
 
-      <section>
-        <h3>Ordering and Payment</h3>
-        <?php include_once "faq/order.html" ?>
-      </section>
+        <section>
+          <?php include_once "faq/order.html" ?>
+        </section>
 
-      <section>
-        <h3>Return and Support</h3>
-        <?php include_once "faq/return.html" ?>
-      </section>
-
-
+        <section>
+          <?php include_once "faq/return.html" ?>
+        </section>
+      </div>
     </div>
+    <?php include "footer/footer.html" ?>
   </div>
-  <?php include "footer/footer.html" ?>
   </div>
 
 
