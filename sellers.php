b08d5d0c5242425ec73da8793f6371ca08395cec
Chibundu <ndubest56@gmail.com>
2025-04-07 05:01:56 +0100

    sellers page

--- sellers.php
+++ sellers.php
@@ -4,14 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="./sellers/seller.css?v=<?php echo time(); ?>">
     <title>Document</title>
 </head>
 
 <body>
     <?php include_once "./header/header.php" ?>
 
-    Register with us
+    <div class="sellercontainer">
+        <div>
+            <?php include_once "./sidebar/sidebar.php" ?>
+        </div>
+        <div class="sellerspage">
+            <div class="sellermain">
+                <h2>Become a Seller</h2>
+                <p class="subtext">Join us and start selling your products to a wider audience.</p>
+                <p class="subtext">Fill out the form below to get started.</p>
+            </div>
+            <?php include_once "footer/footer.html" ?>
+        </div>
+    </div>
+
 
 </body>
 
