f51d9cf2ef0b8d3037a9d5dbc9cf605c472422ec
Chibundu <ndubest56@gmail.com>
2025-04-08 17:10:37 +0100

    products pages

--- productpages/furniture.php
+++ productpages/furniture.php
@@ -5,12 +5,13 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="../main/content.css">
-  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="furniture.css">
   <title>Document</title>
 </head>
 
 <body>
-  <div>
+  <?php include $_SERVER['DOCUMENT_ROOT'] . "/morelinks/header/header.php" ?>
+  <div class="furniture">
     <section class="goods">
       <!-- Display one -->
       <div class="goodsdisplay">
