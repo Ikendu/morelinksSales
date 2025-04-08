9571a7a680368d14cd927525863a199e067862b4
Chibundu <ndubest56@gmail.com>
2025-04-08 20:56:08 +0100

    products pages

--- ask-me.php
+++ ask-me.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="ask.css" />
+  <link rel="stylesheet" href="faq/ask.css" />
   <title>Document</title>
 </head>
 
