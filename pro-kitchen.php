574b485d6f7710237ad6b66add550684255bf4dd
Chibundu <ndubest56@gmail.com>
2025-04-25 16:23:11 +0100

    kitchen furniture

--- pro-kitchen.php
+++ pro-kitchen.php
@@ -116,16 +116,16 @@
 
             <!-- Display fourth -->
             <div class="goodsdisplay">
-                <img src="utilities/images/3.jpg" alt="" />
+                <img src="utilities/images/kit4.jpg" alt="" />
                 <div class="discription">
                     <h4>Excel Space</h4>
                     <p>Living room / cushion / decoration</p>
                     <div class="price">
                         <h5>₦ 4,000,000</h5>
                         <div class="addMore">
-                            <button onclick="updateProductQuantity('Product4', 40, 1, '3.jpg')">+</button>
+                            <button onclick="updateProductQuantity('Product4', 40, 1, 'kit4.jpg')">+</button>
                             <span id="quantityProduct4">0</span>
-                            <button onclick="updateProductQuantity('Product4', 40, -1 , '3.jpg')">-</button>
+                            <button onclick="updateProductQuantity('Product4', 40, -1 , 'kit4.jpg')">-</button>
                         </div>
                     </div>
                     <div class="btnSales">
@@ -137,7 +137,7 @@
                                 <span>Chat us</span>
                             </a>
                         </button>
-                        <button class="cartBtn" id="Product4" onclick="addToCart('Product4', 40, '3.jpg',)">
+                        <button class="cartBtn" id="Product4" onclick="addToCart('Product4', 40, 'kit4.jpg',)">
                             Add to cart
                         </button>
                     </div>
@@ -146,127 +146,7 @@
             <!-- End of Display fourth -->
 
             <!-- Display fifth -->
-            <div class="goodsdisplay">
-                <img src="utilities/images/8.jpg" alt="" />
-                <div class="discription">
-                    <h4>Excel Space</h4>
-                    <p>Living room / cushion / decoration</p>
-                    <div class="price">
-                        <h5>₦ 4,000,000</h5>
-                        <div class="addMore">
-                            <button onclick="updateProductQuantity('Product5', 40, 1, '8.jpg')">+</button>
-                            <span id="quantityProduct5">0</span>
-                            <button onclick="updateProductQuantity('Product5', 40, -1, '8.jpg')">-</button>
-                        </div>
-                    </div>
-                    <div class="btnSales">
-                        <button class="reachUsBtn">
-                            <a
-                                href="https://wa.me/2348061632276?text=Hello%20there!%20I%20need%20a%20furniture"
-                                target="_blank">
-                                <img src="utilities/icons/whatsapp.png" alt="More-Link Whatsapp" />
-                                <span>Chat us</span>
-                            </a>
-                        </button>
-                        <button class="cartBtn" id="Product5" onclick="addToCart('Product5', 40, '8.jpg')">
-                            Add to cart
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <!-- End of Display fifth -->
 
-            <!-- Display sixth -->
-            <div class="goodsdisplay">
-                <img src="utilities/images/9.jpg" alt="" />
-                <div class="discription">
-                    <h4>Excel Space</h4>
-                    <p>Living room / cushion / decoration</p>
-                    <div class="price">
-                        <h5>₦ 4,000,00000</h5>
-                        <div class="addMore">
-                            <button onclick="updateProductQuantity('Product6', 40, 1, '9.jpg')">+</button>
-                            <span id="quantityProduct6">0</span>
-                            <button onclick="updateProductQuantity('Product6', 40, -1, '9.jpg')">-</button>
-                        </div>
-                    </div>
-                    <div class="btnSales">
-                        <button class="reachUsBtn">
-                            <a
-                                href="https://wa.me/2348061632276?text=Hello%20there!%20I%20need%20a%20furniture"
-                                target="_blank">
-                                <img src="utilities/icons/whatsapp.png" alt="More-Link Whatsapp" />
-                                <span>Chat us</span>
-                            </a>
-                        </button>
-                        <button class="cartBtn" id="Product6" onclick="addToCart('Product6', 40, '9.jpg')">
-                            Add to cart
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <!-- End of Display sixth -->
-
-            <!-- Display seventh -->
-            <div class="goodsdisplay">
-                <img src="utilities/images/10.jpg" alt="" />
-                <div class="discription">
-                    <h4>Excel Space</h4>
-                    <p>Living room / cushion / decoration</p>
-                    <div class="price">
-                        <h5>₦ 1,000,000</h5>
-                        <div class="addMore">
-                            <button onclick="updateProductQuantity('Product7', 10, 1, '10.jpg')">+</button>
-                            <span id="quantityProduct7">0</span>
-                            <button onclick="updateProductQuantity('Product7', 10, -1, '10.jpg')">-</button>
-                        </div>
-                    </div>
-                    <div class="btnSales">
-                        <button class="reachUsBtn">
-                            <a
-                                href="https://wa.me/2348061632276?text=Hello%20there!%20I%20need%20a%20furniture"
-                                target="_blank">
-                                <img src="utilities/icons/whatsapp.png" alt="More-Link Whatsapp" />
-                                <span>Chat us</span>
-                            </a>
-                        </button>
-                        <button class="cartBtn" id="Product7" onclick="addToCart('Product7', 10, '10.jpg')">
-                            Add to cart
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <!-- End of Display sevent -->
-
-            <!-- Display eight -->
-            <div class="goodsdisplay">
-                <img src="utilities/images/12.jpg" alt="" />
-                <div class="discription">
-                    <h4>Excel Space</h4>
-                    <p>Living room / cushion / decoration</p>
-                    <div class="price">
-                        <h5>₦ 5,000,000</h5>
-                        <div class="addMore">
-                            <button onclick="updateProductQuantity('Product8', 50, 1, '12.jpg')">+</button>
-                            <span id="quantityProduct8">0</span>
-                            <button onclick="updateProductQuantity('Product8', 50, -1, '12.jpg')">-</button>
-                        </div>
-                    </div>
-                    <div class="btnSales">
-                        <button class="reachUsBtn">
-                            <a
-                                href="https://wa.me/2348061632276?text=Hello%20there!%20I%20need%20a%20furniture"
-                                target="_blank">
-                                <img src="utilities/icons/whatsapp.png" alt="More-Link Whatsapp" />
-                                <span>Chat us</span>
-                            </a>
-                        </button>
-                        <button class="cartBtn" id="Product8" onclick="addToCart('Product8', 50, '12.jpg')">
-                            Add to cart
-                        </button>
-                    </div>
-                </div>
-            </div>
             <!-- End of Display eight -->
         </section>
     </div>
