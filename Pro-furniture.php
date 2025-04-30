9bf8737dbfa3bf31f60cb7a836b458e3b2ba33c2
Chibundu <ndubest56@gmail.com>
2025-04-30 16:19:53 +0100

    tiltle and others

--- Pro-furniture.php
+++ Pro-furniture.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="main/content.css">
   <link rel="stylesheet" href="productutilities/furniture.css">
-  <title>Document</title>
+  <title>More-links - Furniture</title>
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Lato|Cinzel|Fauna+One" rel="stylesheet">
     <link rel="stylesheet" href="main/styles.css?&<?php time() ?>">
-    <title>More-links</title>
+    <title>More-links - Home</title>
 </head>
 
 <body>
