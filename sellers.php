c6ce12e16292eb500a27ef26ba154da514a2f48e
Chibundu <ndubest56@gmail.com>
2025-04-30 16:24:13 +0100

    tiltle and others

--- sellers.php
+++ sellers.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./sellers/seller.css?v=<?php echo time(); ?>">
-    <title>Document</title>
+    <title>More-links - Sellers Form</title>
 </head>
 
 <body>
--- services.php
+++ services.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="services/services.css" />
-  <title>Document</title>
+  <title>More-links - Service</title>
 </head>
 
 <body>
